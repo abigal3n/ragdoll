dffa051362db39a5a6c0b77b9d706957b0d4ce32
abigal3n <abbykittygaming13@gmail.com>
2025-05-03 13:52:50 -0400

    head rotates too

--- main.lua
+++ main.lua
@@ -13,9 +13,12 @@ alpha3 = 0
 alpha4 = 0
 omega3 = 0
 omega4 = 0
+headAlpha = 0
+headOmega = 0
 armFg = armMass*gravity
 headWidth = 200
 headHeight = 200
+headMass = 50
 
 function getTorque(dist,force,theta)
     angle = math.sin(-theta)
@@ -41,7 +44,7 @@ arm1 = {mode="fill", x=torso.x-armWidth, y=torso.y, width=armWidth, height=armLe
 arm2 = {mode="fill", x=torso.x+torso.width, y=torso.y, width=armWidth, height=armLength, theta=math.rad(20)}
 leg1 = {mode="fill", x=torso.x, y=torso.y+torso.height, width=legWidth, height=legLength, theta= math.rad(-20)}
 leg2 = {mode="fill", x=torso.x+torso.width-legWidth, y=torso.y+torso.height, width=legWidth, height=legLength, theta=math.rad(20)}
-catObj = {img = "scaredcat.png", x=(torso.x+(torso.width/2)-70),y=torso.y-80, width=headWidth, height=headHeight}
+catObj = {img = "scaredcat.png", x=(torso.x+(torso.width/2)-70),y=torso.y-80, width=headWidth, height=headHeight, theta=math.rad(-20)}
 function drawRotatedRectangle(mode,x,y,w,h,theta)
     love.graphics.push()
 	love.graphics.translate(x, y)
@@ -49,6 +52,14 @@ function drawRotatedRectangle(mode,x,y,w,h,theta)
 	love.graphics.rectangle(mode, 0, 0, w, h)
     love.graphics.pop()
 end
+function drawRotatedImage(img,x,y,theta, w)
+    love.graphics.push()
+    love.graphics.translate(x+(w/2)-30,y)
+    love.graphics.rotate(theta)
+    love.graphics.draw(img, (-w/2)+30, 0, 0, 0.2, 0.2)
+    love.graphics.pop()
+end
+
 function drawLeftArm(mode,x,y,w,h,theta)
     love.graphics.push()
     love.graphics.translate(x+w,y)
@@ -70,6 +81,12 @@ function love.load()
     smallFont = love.graphics.newFont(10)
 end
 function love.update(dt)
+    if love.keyboard.isDown("w") then
+        torso.y = torso.y - 10 *dt
+    end
+    if love.keyboard.isDown("s") then
+        torso.y = torso.y + 10 *dt
+    end
     if love.keyboard.isDown("r") then
         arm1.theta = arm1.theta + omega * dt
     end
@@ -83,10 +100,13 @@ function love.update(dt)
     omega3 = omega3 + alpha3 * dt
     alpha4 = getAlpha(legLength/2, leg2.theta, legMass, legLength)
     omega4 = omega4 + alpha4 * dt
+    headAlpha = getAlpha(headWidth/2, catObj.theta, headMass, headWidth)
+    headOmega = headOmega + headAlpha * dt
     arm1.theta = arm1.theta + omega1* dt
     arm2.theta = arm2.theta + omega2* dt
     leg1.theta = leg1.theta + omega3* dt
     leg2.theta = leg2.theta + omega4* dt
+    catObj.theta = catObj.theta + headOmega* dt
     sin = math.sin(-arm1.theta)
 end
 function love.draw()
@@ -100,5 +120,6 @@ function love.draw()
     drawRightArm(arm2.mode,arm2.x,arm2.y,arm2.width,arm2.height,arm2.theta)
     drawRotatedRectangle(leg1.mode,leg1.x,leg1.y,leg1.width,leg1.height,leg1.theta)
     drawRotatedRectangle(arm2.mode,leg2.x,leg2.y,leg2.width,leg2.height,leg2.theta)
-    love.graphics.draw(cat, catObj.x, catObj.y, 0, 0.2, 0.2)
+    drawRotatedImage(cat, catObj.x, catObj.y, catObj.theta, headWidth)
+    --love.graphics.draw(cat, catObj.x, catObj.y, 0, 0.2, 0.2)
 end
